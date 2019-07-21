5a6ff2657a21d06e8b7e8c87dc936d2c189e4a24
Allison Tearjen <amtearjen@ucdavis.edu>
2019-07-20 18:09:42 -0700

    added shared pref

--- flutter_idk/lib/main.dart
+++ flutter_idk/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:date_utils/date_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // Example holidays
 final Map<DateTime, List> _holidays = {
@@ -23,14 +24,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Table Calendar Demo',
+      title: 'Tracker Calendar',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Table Calendar Demo'),
+      home: MyHomePage(title: 'Tracker Calendar'),
     );
   }
 }
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -54,21 +56,21 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     super.initState();
     _selectedDay = DateTime.now();
     _events = {
-      _selectedDay.subtract(Duration(days: 30)): ['Event A0', 'Event B0', 'Event C0'],
-      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 20)): ['Event A2', 'Event B2', 'Event C2', 'Event D2'],
-      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
-      _selectedDay.subtract(Duration(days: 10)): ['Event A4', 'Event B4', 'Event C4'],
-      _selectedDay.subtract(Duration(days: 4)): ['Event A5', 'Event B5', 'Event C5'],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
-      _selectedDay.add(Duration(days: 1)): ['Event A8', 'Event B8', 'Event C8', 'Event D8'],
-      _selectedDay.add(Duration(days: 3)): Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
-      _selectedDay.add(Duration(days: 7)): ['Event A10', 'Event B10', 'Event C10'],
-      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
-      _selectedDay.add(Duration(days: 17)): ['Event A12', 'Event B12', 'Event C12', 'Event D12'],
-      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
-      _selectedDay.add(Duration(days: 26)): ['Event A14', 'Event B14', 'Event C14'],
+//      _selectedDay.subtract(Duration(days: 30)): ['Event A0', 'Event B0', 'Event C0'],
+//      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
+//      _selectedDay.subtract(Duration(days: 20)): ['Event A2', 'Event B2', 'Event C2', 'Event D2'],
+//      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
+//      _selectedDay.subtract(Duration(days: 10)): ['Event A4', 'Event B4', 'Event C4'],
+//      _selectedDay.subtract(Duration(days: 4)): ['Event A5', 'Event B5', 'Event C5'],
+//      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+//      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+//      _selectedDay.add(Duration(days: 1)): ['Event A8', 'Event B8', 'Event C8', 'Event D8'],
+//      _selectedDay.add(Duration(days: 3)): Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+//      _selectedDay.add(Duration(days: 7)): ['Event A10', 'Event B10', 'Event C10'],
+//      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
+//      _selectedDay.add(Duration(days: 17)): ['Event A12', 'Event B12', 'Event C12', 'Event D12'],
+//      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
+//      _selectedDay.add(Duration(days: 26)): ['Event A14', 'Event B14', 'Event C14'],
     };
 
     _selectedEvents = _events[_selectedDay] ?? [];
@@ -122,6 +124,16 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           // Switch out 2 lines below to play with TableCalendar's settings
           //-----------------------
           _buildTableCalendar(),
+          RaisedButton(
+            child: Text("Add Today's Log"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondRoute()),
+                );
+              // Navigate to second route when tapped.
+              },
+          ),
           // _buildTableCalendarWithBuilders(),
           const SizedBox(height: 8.0),
           Expanded(child: _buildEventList()),
@@ -129,6 +141,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       ),
     );
   }
+
 
   // Simple TableCalendar configuration (using Styles)
   Widget _buildTableCalendar() {
@@ -161,6 +174,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
       onVisibleDaysChanged: _onVisibleDaysChanged,
     );
   }
+
 
   // More advanced TableCalendar configuration (using Builders & Styles)
   Widget _buildTableCalendarWithBuilders() {
@@ -301,3 +315,226 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     );
   }
 }
+
+class SecondRoute extends StatefulWidget {
+  @override
+  _SecondRouteState createState() => new _SecondRouteState();
+}
+
+
+class _SecondRouteState extends State<SecondRoute> {
+  TextEditingController facebookTime = new TextEditingController();
+  TextEditingController instagramTime = new TextEditingController();
+  TextEditingController moodScore = new TextEditingController();
+
+  bool checkValue = false;
+
+  SharedPreferences sharedPreferences;
+
+  @override
+  void initState() {
+    super.initState();
+    getInformation();
+  }
+  getInformation() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {
+      checkValue = sharedPreferences.getBool("check");
+      if (checkValue != null) {
+        if (checkValue) {
+          facebookTime.text = sharedPreferences.getString("facebook");
+          instagramTime.text = sharedPreferences.getString("instagram");
+        } else {
+          facebookTime.clear();
+          instagramTime.clear();
+          sharedPreferences.clear();
+        }
+      } else {
+        checkValue = false;
+      }
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white12,
+      ),
+      body: new SingleChildScrollView(
+        child: _body(),
+        scrollDirection: Axis.vertical,
+      ),
+    );
+  }
+  _onChanged(bool value) async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {
+      checkValue = value;
+      sharedPreferences.setString("facebook", facebookTime.text);
+      sharedPreferences.setString("instagram", instagramTime.text);
+      sharedPreferences.setString("mood", moodScore.text);
+//      sharedPreferences.commit();
+      getInformation();
+    });
+  }
+
+  Widget _body() {
+    return new Container(
+      padding: EdgeInsets.only(right: 20.0, left: 20.0),
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+
+          new TextField(
+            controller: facebookTime,
+            decoration: new InputDecoration(labelText: "Facebook Time (in minutes)", hintText: "50"),
+            keyboardType: TextInputType.number,
+            //controller: userCtrl,
+//                decoration: InputDecoration(
+//                    hintText: "45",
+//                    contentPadding: const EdgeInsets.all(20)),
+            style: TextStyle(
+              color: Colors.grey[850],
+              fontSize: 20.0,
+              fontFamily: "WorkSans",
+            ),
+          ),
+
+          TextField(
+            controller: instagramTime,
+            decoration: new InputDecoration(labelText: "Instagram Time (in minutes)", hintText: "45"),
+            keyboardType: TextInputType.number,
+            //controller: userCtrl,
+//                decoration: InputDecoration(
+//                    hintText: "60",
+//                    contentPadding: const EdgeInsets.all(20)),
+            style: TextStyle(
+              color: Colors.grey[850],
+              fontSize: 20.0,
+              fontFamily: "WorkSans",
+            ),
+
+          ),
+          new TextField(
+            controller: moodScore,
+
+            decoration: new InputDecoration(labelText: "Mood Score out of 10", hintText: "9 :)"),
+            keyboardType: TextInputType.number,
+            //controller: userCtrl,
+//                decoration: InputDecoration(
+//                    hintText: "60",
+//                    contentPadding: const EdgeInsets.all(20)),
+            style: TextStyle(
+              color: Colors.grey[850],
+              fontSize: 20.0,
+              fontFamily: "WorkSans",
+            ),
+          ),
+          new RaisedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+              ),
+          new RaisedButton(
+                onPressed: () {
+                  _onChanged(checkValue);
+
+                  Navigator.pop(context);
+                },
+                  // go back but submit
+                child: Text("Submit")
+              ),
+
+        ],
+      ),
+    );
+  }
+
+}
+
+//class SecondRoute extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text("Daily Tracker Form"),
+//      ),
+//      body: Center(
+//          child: Column(
+//            children: <Widget>[
+//
+//              TextField(
+//                controller: facebookTime,
+//                decoration: new InputDecoration(labelText: "Facebook Time (in minutes)", hintText: "50"),
+//                keyboardType: TextInputType.number,
+//                //controller: userCtrl,
+////                decoration: InputDecoration(
+////                    hintText: "45",
+////                    contentPadding: const EdgeInsets.all(20)),
+//                style: TextStyle(
+//                  color: Colors.grey[850],
+//                  fontSize: 20.0,
+//                  fontFamily: "WorkSans",
+//                ),
+//              ),
+//
+//              TextField(
+//                controller: instagramTime,
+//                decoration: new InputDecoration(labelText: "Instagram Time (in minutes)", hintText: "45"),
+//                keyboardType: TextInputType.number,
+//                //controller: userCtrl,
+////                decoration: InputDecoration(
+////                    hintText: "60",
+////                    contentPadding: const EdgeInsets.all(20)),
+//                style: TextStyle(
+//                  color: Colors.grey[850],
+//                  fontSize: 20.0,
+//                  fontFamily: "WorkSans",
+//                ),
+//
+//              ),
+//              TextField(
+//                controller: moodScore,
+//                decoration: new InputDecoration(labelText: "Mood Score out of 10", hintText: "9 :)"),
+//                keyboardType: TextInputType.number,
+//                //controller: userCtrl,
+////                decoration: InputDecoration(
+////                    hintText: "60",
+////                    contentPadding: const EdgeInsets.all(20)),
+//                style: TextStyle(
+//                  color: Colors.grey[850],
+//                  fontSize: 20.0,
+//                  fontFamily: "WorkSans",
+//                ),
+//
+//              ),
+//              RaisedButton(
+//                onPressed: () {
+//                  Navigator.pop(context);
+//                },
+//                child: Text("Cancel"),
+//              ),
+//              RaisedButton(
+//                onPressed: () {
+//
+//                  Navigator.pop(context);
+//                },
+//                  // go back but submit
+//                child: Text("Submit")
+//              ),
+//              Expanded(
+//                child: FittedBox(
+//                  fit: BoxFit.contain, // otherwise the logo will be tiny
+////                  child: const FlutterLogo(),
+//                ),
+//              ),
+//            ],
+//          )
+//
+//        )
+//      );
+//  }
+//
+//}
